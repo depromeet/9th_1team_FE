21ba12ee8c43edad9fb5bb1008195d47a13a763c
KIM JUNGHYUN <richg0ld@op.gg>
2021-04-12 11:58:25 +0900

    기존 릴레이 세팅 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,10 @@
-import styled from 'styled-components'
-import { RelayEnvironmentProvider } from "react-relay/hooks"
-import environment from "../relay-env"
-import TestComponent from '../TestComponent'
+import styled from "styled-components";
 
 const Title = styled.h1`
   color: red;
   font-size: 50px;
-`
+`;
 
 export default function Home() {
-  return (
-  <RelayEnvironmentProvider environment={environment}>
-    <Title>My page</Title>
-    <TestComponent />
-  </RelayEnvironmentProvider>
-  )
-  
+  return <Title>My page</Title>;
 }
