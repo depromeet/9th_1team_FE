e72704892b39a916bda38093fb35e62b768c06c2
KIM JUNGHYUN <richg0ld@op.gg>
2021-04-21 21:23:49 +0900

    내용, 서브 텍스트 기능 추가

--- pages/article/write/index.tsx
+++ pages/article/write/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import Modal from "react-modal";
 import { SketchPicker } from "react-color";
 
@@ -6,13 +6,23 @@ Modal.setAppElement("#__next");
 
 const Write = () => {
   const [isOpenModal, setIsOpenModal] = useState(false);
-  const [isOpenColor, setIsOpenColor] = useState(false);
+  const [isOpenColor, setIsOpenColorModal] = useState(false);
+  const [isOpenSubText, setIsOpenSubText] = useState(false);
   const [balanceId, setBalanceId] = useState("");
   const [color, setColor] = useState({});
+  const [text, setText] = useState("");
+  const [subText, setSubText] = useState("");
+  const [BallanceDataA, setBallanceDataA] = useState(null);
+  const [BallanceDataB, setBallanceDataB] = useState(null);
 
   const onClose = () => {
     setIsOpenModal(false);
     setBalanceId("");
+  };
+
+  const onColorClose = () => {
+    setIsOpenColorModal(false);
+    setColor("");
   };
 
   const onOpenBalance = (balanceId: string) => () => {
@@ -20,25 +30,62 @@ const Write = () => {
     setBalanceId(balanceId);
   };
 
-  const handleChangeComplete = (color) => {
+  const handleChangeComplete = (color: string) => {
     setColor(color);
   };
+
+  const onAddSubText = () => {
+    setIsOpenSubText(true);
+  };
+
+  const onRemoveSubText = () => {
+    setIsOpenSubText(false);
+  };
+
+  const onText = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const onSubText = (e: ChangeEvent<HTMLInputElement>) => {
+    setSubText(e.target.value);
+  };
+
+  const onSuccess = () => {};
 
   return (
     <>
       <Modal isOpen={isOpenModal} onRequestClose={onClose}>
         <div>선택지 {balanceId}</div>
         <div>
-          <input placeholder="내용을 입력해주세요" />
-          <input placeholder="내용을 입력해주세요" />
+          <input
+            placeholder="내용을 입력해주세요"
+            value={text}
+            onChange={onText}
+          />
+          {isOpenSubText && (
+            <input
+              placeholder="서브 텍스트 내용을 입력해주세요"
+              value={subText}
+              onChange={onSubText}
+            />
+          )}
         </div>
         <div>
-          <button>서브 텍스트 추가하기</button>
-          <button onClick={() => setIsOpenColor(true)}>텍스트 색상 변경</button>
-          <button>사진 추가</button>
+          {!isOpenSubText ? (
+            <button onClick={onAddSubText}>서브 텍스트 추가하기</button>
+          ) : (
+            <button onClick={onRemoveSubText}>서브 텍스트 삭제</button>
+          )}
+          <button onClick={() => setIsOpenColorModal(true)}>
+            텍스트 색상 변경
+          </button>
+          <input type="file" />
+        </div>
+        <div>
+          <button onClick={onSuccess}>완료</button>
         </div>
       </Modal>
-      <Modal isOpen={isOpenColor}>
+      <Modal isOpen={isOpenColor} onRequestClose={onColorClose}>
         <SketchPicker
           color={color.hex}
           onChangeComplete={handleChangeComplete}
