1b20329d5c127dedc177ed35cf8175c5637bbc93
KIM JUNGHYUN <richg0ld@op.gg>
2021-05-20 00:37:20 +0900

    콘솔제거

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -18,7 +18,6 @@ function createApolloClient() {
   const authLink = setContext((_, { headers }) => {
     // get the authentication token from local storage if it exists
     const token = localStorage.getItem("token");
-    console.log("??????????", token);
     // return the headers to the context so httpLink can read them
     return {
       headers: {
