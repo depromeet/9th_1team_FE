48b1a6a803ba560f9dd06ef6d38fa052218986a2
yujin mac <kimyu28jin@gmail.com>
2021-05-28 17:58:40 +0900

    NotLogin 페이지 스타일 수정

--- components/MypageContent/NotLogin.tsx
+++ components/MypageContent/NotLogin.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Container, BtnContainer, ContentContainer } from "./NotLogin.style";
 import CommonHeader from "../Header/CommonHeader";
 import Link from "next/link";
+import VomitIcon from "../../public/tomato-vomit.svg";
+import NextArr from "../../public/game-next.svg";
 
 const MiniCards: React.FC = () => {
   return (
@@ -9,13 +11,18 @@ const MiniCards: React.FC = () => {
       <CommonHeader title={"마이페이지"} />
       <BtnContainer>
         <Link href={"/login"}>
-          <a className="login">로그인/회원가입</a>
+          <a className="login">
+            <span>로그인/회원가입</span>
+            <NextArr />
+          </a>
         </Link>
       </BtnContainer>
       <ContentContainer>
-        <img src="img.png" alt="" />
-        로그인 후 <br />
-        밸런스 게임을 만들어보세요!
+        <VomitIcon />
+        <p>
+          로그인 후 <br />
+          밸런스 게임을 만들어보세요!
+        </p>
       </ContentContainer>
     </Container>
   );
