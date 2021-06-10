847f98ebe1b39cc1b672f443c491b384a9c7aa81
yujin mac <kimyu28jin@gmail.com>
2021-06-10 19:57:37 +0900

    ColorPicker 스타일 수정

--- components/ColorPicker.tsx
+++ components/ColorPicker.tsx
@@ -2,13 +2,25 @@ import React, { Dispatch, SetStateAction } from "react";
 import styled from "styled-components";
 
 const PickerWrapper = styled.div<{ picked: boolean }>`
-  width: 3.2rem;
-  height: 3.2rem;
+  width: 3.6rem;
+  height: 3.6rem;
   display: inline-block;
   border-radius: 0.8rem;
-  border: ${({ picked }) => (picked ? "4px solid #fff" : "2px solid #fff")};
+  position: relative;
   box-shadow: ${({ picked }) =>
     picked ? "0px 2px 2px rgba(0, 0, 0, 0.1)" : "none"};
+`;
+
+const Border = styled.div<{ picked: boolean }>`
+  width: 100%;
+  height: 100%;
+  display: inline-block;
+  position: absolute;
+  left: 0;
+  top: 0;
+  border-radius: 0.8rem;
+  box-shadow: ${({ picked }) =>
+    picked ? "0 0 0 4px #fff inset" : "0 0 0 2px #fff inset"};
 `;
 
 const ColorWrapper = styled.div`
@@ -47,6 +59,7 @@ const ColorPicker: React.FC<colorProps> = ({
   };
   return (
     <PickerWrapper onClick={onClickPicker} picked={picked}>
+      <Border picked={picked} />
       <ColorWrapper>
         <Color color={colorA}></Color>
         <Color color={colorB}></Color>
