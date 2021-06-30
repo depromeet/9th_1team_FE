d125925a43eb78a776d421fda152bc36faf223e1
yujin mac <kimyu28jin@gmail.com>
2021-06-30 14:38:35 +0900

    상세 query 분리

--- lib/queries.ts
+++ lib/queries.ts
@@ -18,3 +18,66 @@ export const MY_GAMES = gql`
     }
   }
 `;
+
+export const GET_GAME = gql`
+  query balanceGameLogined($id: String!) {
+    balanceGame: balanceGameLogined(id: $id) {
+      id
+      userId
+      description
+      mySelection
+      commentCount
+      createdAt
+      user {
+        profile {
+          nickname
+        }
+      }
+      balanceGameSelections {
+        id
+        order
+        voteCount
+        balanceGameId
+        backgroundImage
+        backgroundColor
+        textColor
+        description
+      }
+    }
+  }
+`;
+
+export const GET_GAME_NOT_LOGIN = gql`
+  query balanceGameNotLogined($id: String!) {
+    balanceGame: balanceGameNotLogined(id: $id) {
+      id
+      userId
+      description
+      mySelection
+      commentCount
+      createdAt
+      user {
+        profile {
+          nickname
+        }
+      }
+      balanceGameSelections {
+        id
+        order
+        balanceGameId
+        backgroundImage
+        backgroundColor
+        textColor
+        description
+      }
+    }
+  }
+`;
+
+export const NEXT_GAME_BY_RANDOM_QUERY = gql`
+  query nextGameByRandom {
+    nextGameByRandom {
+      id
+    }
+  }
+`;
