bf6e8a97971b57e7c5b7bb19c3572e5af7f9d3b8
yujin mac <kimyu28jin@gmail.com>
2021-07-08 15:36:54 +0900

    infinite scroll 수정, 코드정리

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,6 @@ import {
   BALANCE_GAMES_LOGINED_QUERY,
   BALANCE_GAMES_QUERY,
   BALANCE_GAMES_TICK,
-  GET_GAME,
-  GET_GAME_NOT_LOGIN,
   NEXT_GAME_BY_RANDOM_QUERY,
 } from "lib/queries";
 import Header from "components/Header";
@@ -35,9 +33,7 @@ const Index: React.FC<IndexProps> = ({ isLoggedin }) => {
   const [updateLoading, setUpdateLoading] = useState(false);
   const [offset, setOffset] = useState(0);
   const [hasMore, setHasMore] = useState(true);
-  const [isLoaded, setIsLoaded] = useState(false);
   const [isFiltered, setIsFiltered] = useState(false);
-  const [list, setList] = useState([]);
   const [qBalanceGames, { loading, data, refetch: loadGameFeed }] = useLazyQuery(
     isLoggedin ? BALANCE_GAMES_LOGINED_QUERY : BALANCE_GAMES_QUERY,
   );
@@ -58,22 +54,18 @@ const Index: React.FC<IndexProps> = ({ isLoggedin }) => {
     const newList = data?.balanceGames?.balanceGames;
     // 새로 들어온 데이터인지 확인(투표한 데이터인 경우 중복렌더링 방지)
     let isOldData = listData?.some((item: any) => item.id === newList[0].id);
-    // 투표한 데이터일 경우 다시 데이터를 세팅하고 종료
-    if (isOldData) {
-      console.log("투표데이터 들어옴!!--->>>>", newList);
 
-      // 투표한 데이터와 기존 데이터 비교해서 list의 정보 업데이트 하면될듯
-      console.log("load~!@##&$*(&@$(@&#*@(^#@(");
-      return;
-    }
+    // 투표한 데이터일 경우 종료
+    if (isOldData) return;
 
     // 새로 들어온 데이터일 경우
-    if (newList.length < 1) setHasMore(false);
-    else {
+    if (newList.length < 1) {
+      setHasMore(false);
+    } else {
       dispatch(addList(newList));
       if (newList.length < BALANCE_GAMES_TICK) setHasMore(false);
     }
-  }, [data, data?.balanceGames?.balanceGames]);
+  }, [data, data?.balanceGames?.balanceGames, hasMore]);
 
   if (_.isEmpty(listData)) return null;
 
@@ -112,16 +104,16 @@ const Index: React.FC<IndexProps> = ({ isLoggedin }) => {
             {isFiltered ? <Select /> : <Unselect />}
           </Participate>
         </div>
-        <InfiniteScroll
-          dataLength={list.length}
-          next={fetchMoreData}
-          style={{ fontSize: 0 }} //To put endMessage and loader to the top.
-          // inverse={true} //
-          hasMore={hasMore}
-          loader={<Loading />}
-        >
-          {listData &&
-            listData.map((data, i) => (
+        {listData && (
+          <InfiniteScroll
+            dataLength={listData.length}
+            next={fetchMoreData}
+            style={{ fontSize: 0 }}
+            // inverse={true} //
+            hasMore={hasMore}
+            loader={<Loading />}
+          >
+            {listData.map((data, i) => (
               <FeedPost
                 key={i}
                 updateLoading={updateLoading}
@@ -129,7 +121,8 @@ const Index: React.FC<IndexProps> = ({ isLoggedin }) => {
                 data={data}
               />
             ))}
-        </InfiniteScroll>
+          </InfiniteScroll>
+        )}
       </Container>
     </div>
   );
